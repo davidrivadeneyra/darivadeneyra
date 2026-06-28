68831cfdc800166b12eee54e599d6bdeed571768
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2026-06-27 21:11:12 -0500

    mejoras en el open graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL ?? 'https://davidrivadeneyra.com',
   server: {
     host: '127.0.0.1',
     port: 3001
